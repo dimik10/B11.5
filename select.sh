9a297377594ce82f6314dbfe74abbc1da2fe02a8
dimik10 <38129732+dimik10@users.noreply.github.com>
2023-04-09 11:35:09 +0300

    Update select.sh

--- select.sh
+++ select.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam <<MY_QUERY
-select * from fr.fram_acc limit 1;
-MY_QUERY
+mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam <<QUERY
+SELECT fr.rfam_acc, fr.rfamseq_acc, fr.seq_start, fr.seq_end
+FROM full_region fr, rfamseq rf, taxonomy tx
+WHERE rf.ncbi_id = tx.ncbi_id
+AND fr.rfamseq_acc = rf.rfamseq_acc
+AND tx.ncbi_id = 10116 -- NCBI taxonomy id of Rattus norvegicus
+AND is_significant = 1 -- exclude low-scoring matches from the same clan
+QUERY
