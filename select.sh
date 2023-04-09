7ec282f36feb73a9512697b02eaa2a6bd848f93e
Dmitriy Nikolaev <dimik10@mail.ru>
2023-04-09 12:07:28 +0300

    Change

--- select.sh
+++ select.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam <<QUERY_DB
+mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam <<QUERY_DB1
 SELECT fr.rfam_acc, fr.rfamseq_acc, fr.seq_start, fr.seq_end
 FROM full_region fr, rfamseq rf, taxonomy tx
 WHERE rf.ncbi_id = tx.ncbi_id
 AND fr.rfamseq_acc = rf.rfamseq_acc
 AND tx.ncbi_id = 10116 -- NCBI taxonomy id of Rattus norvegicus
 AND is_significant = 1 -- exclude low-scoring matches from the same clan
-QUERY_DB
+QUERY_DB1
